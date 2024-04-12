bf799174cbdbd891ab3a5d243eb45dd1b0a20fcd
shahnoorgit <shahnoormujawar@gmail.com>
2024-04-12 11:57:38 +0530

    bug #2

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
+// next.config.js
+
 const nextConfig = {
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
   images: {
     domains: ["lh3.googleusercontent.com"],
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    return config;
   },
 };
 
